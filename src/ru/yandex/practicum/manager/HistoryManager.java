f63bc0c5e7e179a7f48736fa37b276d395ced9a0
Ilya Kravchenko <kravchi0803@gmail.com>
2024-04-27 15:19:03 +0300

    version 1: change structure of InMemoryHistoryManager. Make it as custom parameterized LinkedList and add its methods

--- src/ru/yandex/practicum/manager/HistoryManager.java
+++ src/ru/yandex/practicum/manager/HistoryManager.java
@@ -4,8 +4,8 @@ import ru.yandex.practicum.tasks.Task;
 
 import java.util.List;
 
-public interface HistoryManager {
-    void addToHistory(Task task);
-
-    List<Task> getHistory();
+public interface HistoryManager<T extends Task> {
+    void addToHistory(T instance);
+    void removeFromHistory(int id);
+    List<T> getHistory();
 }
--- src/ru/yandex/practicum/manager/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/manager/InMemoryHistoryManager.java
@@ -2,24 +2,70 @@ package ru.yandex.practicum.manager;
 
 import ru.yandex.practicum.tasks.Task;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class InMemoryHistoryManager implements HistoryManager {
-    private static final int HISTORY_LIMIT = 10;
-
-    private final List<Task> history = new LinkedList<>();
+public class InMemoryHistoryManager<T extends Task> implements HistoryManager<T> {
+    private final Map<Integer, Node<T>> history = new HashMap<>();
+    private Node<T> head;
+    private Node<T> tail;
 
     @Override
-    public void addToHistory(Task task) {
-        if (history.size() == HISTORY_LIMIT) {
-            history.remove(0);
+    public void addToHistory(T instance) {
+        if (history.containsKey(instance.getId())) {
+            removeFromHistory(instance.getId());
         }
-        history.add(task);
+        linkLast(instance);
     }
 
     @Override
-    public List<Task> getHistory() {
-        return history;
+    public void removeFromHistory(int id) {
+        removeNode(history.get(id));
+    }
+
+    @Override
+    public List<T> getHistory() {
+        return getTasks();
+    }
+
+    private void linkLast(T instance) {
+        Node<T> oldTail = tail;
+        Node<T> newNode = new Node<>(oldTail, null, instance);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+        history.put(instance.getId(), newNode);
+    }
+
+    private List<T> getTasks() {
+        List<T> list = new ArrayList<>();
+        Node<T> node = head;
+        while (node != null) {
+            list.add(node.instance);
+            node = node.next;
+        }
+        return list;
+    }
+
+    private void removeNode(Node<T> node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    class Node<E> {
+        Node<E> prev;
+        Node<E> next;
+        E instance;
+
+        public Node(Node<E> prev, Node<E> next, E instance) {
+            this.prev = prev;
+            this.next = next;
+            this.instance = instance;
+        }
     }
 }
